e0c7e12b15ca1f019b4048e94e30734469e2f138
umeshmad <kumdu284@gmail.com>
2026-08-17 01:56:55 +0530

    start the backend

--- archtech-backend/lib/db.js
+++ archtech-backend/lib/db.js
@@ -1,10 +1,9 @@
-import mysql from 'mysql2/promise';
-
+import mysql from "mysql2/promise";
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'root',
-  database: process.env.DB_NAME || 'archtech',
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "archtech_pro",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
